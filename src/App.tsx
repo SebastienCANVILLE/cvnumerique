893de493583ff925edc4e476c02e1749fc274d95
CANVILLE Sebastien (SNCF / SURETE FERROVIAIRE / SUGE BORDEAUX) <8407285d@commun.ad.sncf.fr>
2023-02-23 11:48:08 +0100

    feat components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Accueil from './components/Accueil/accueil';
+import CvAccueil from './components/CV/cvAccueil';
 
 function App() {
   return (
---
+++ src/components/CV/cvAccueil.tsx
@@ -0,0 +1,12 @@
+import Presentation from "./presentation";
+import ProfilCv from "./profil";
+import Experience from "./experiences";
+import CentreInteret from "./centresInterets";
+import Fonctionnelle from "./fonctionnelles";
+import Formation from "./formations";
+import Langue from "./langues";
+import Navbar from "./navbar";
+import Technique from "./techniques";
+import Competence from "./competences";
+
+export default function CvAccueil () {}
