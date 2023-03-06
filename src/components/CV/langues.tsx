7dfbedef2315a262a7b63f740dba7b846d29a933
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-03-06 16:30:46 +0100

    fix CreateLangue

--- src/components/CV/langues.tsx
+++ src/components/CV/langues.tsx
@@ -9,17 +9,21 @@ type TLangues = {
 
 export default function CreateLangue () {
     const [langues, setLangues] = useState <TLangues |undefined > ();
+    const [niveau, SetNiveau] = useState <TLangues |undefined >();
     const [languesInput, setLanguesInput] = useState ("");
+    const [niveauInput, SetNiveauInput] = useState ("");
 
+    //useEffect (() => {
     async function fetchData() {
         const body = {
-            langue: languesInput
+            langue: languesInput,
+            niveau: niveauInput
         }
         const requestOptions = {
             method: 'POST',
             headers: {
-                'content-Type': 'application/json',
-                Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNBTkRSSU5FQGdtYWlsLmZyIiwic3ViIjoxNywiaWF0IjoxNjc2OTkzNzk4LCJleHAiOjE2ODE5OTM3OTh9.kHWzfvF0s4Wyb_x8COEX9p344z2SG63EoZ5KJ9uv21o'
+                'Content-Type': 'application/json',
+                Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODExMzgwOSwiZXhwIjoxNjgzMTEzODA5fQ.SGeVdA_5QzUAfYmwy8dYn0MueTm6p6f7mVsbuGKReys'
             },
             body: JSON.stringify(body)
         };
@@ -29,15 +33,25 @@ export default function CreateLangue () {
 
         setLangues(responseJson);
         console.log(langues);
+        SetNiveau(responseJson);
+        console.log(niveau);        
     }
+    //fetchData();
+//},[]);
 
     return (
         <div>
             <div className="card text-center m-3">
+                <h5 className="card-title">Connaissance d'une langue</h5>
                 <h5 className="card-header">{langues?.langue}</h5>
                 <input type='text' onChange={(event) => setLanguesInput(event.target.value)}></input>
+                
+                <h5 className="card-title">Mon niveau de connaissance de la langue:</h5>
+                <h5 className="card-header"> {langues?.niveau} </h5>
+                <input type='text' onChange={(event) => SetNiveauInput(event.target.value)}></input>
                 <button onClick={fetchData}>Valider</button>
             </div>
         </div>
     );
 }
+export { CreateLangue}
