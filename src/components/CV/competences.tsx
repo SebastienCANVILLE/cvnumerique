93f1f2a2af397926599265857959e14956222843
WebToLaure <lmarmus@hotmail.fr>
2023-03-07 11:36:28 +0100

    feat bootstrap icons et techniques

--- src/components/CV/competences.tsx
+++ src/components/CV/competences.tsx
@@ -33,10 +33,11 @@ export default function Competence() {
 
     return (
         <div className='container mt-5'>
+            <div className="accordion" id="accordionPanelsStayOpenExample">
             <div className="accordion-item">
                 <h2 className="accordion-header" id="panelsStayOpen-headingSeven">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseSeven" aria-expanded="false" aria-controls="panelsStayOpen-collapseSeven">
-                        Compétences clés
+                        COMPETENCES CLES
                     </button>
                 </h2>
                 <div id="panelsStayOpen-collapseSeven" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingSeven">
@@ -46,6 +47,7 @@ export default function Competence() {
                     <div className="accordion-body">
                         <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                     </div>
+                </div>
                 </div>
             </div>
         </div>
--- src/components/CV/formations.tsx
+++ src/components/CV/formations.tsx
@@ -70,14 +70,15 @@ export default function Formation() {
 
         }, []); */
     }
-        return (
+    return (
 
 
-            <div className='container mt-5'>
+        <div className='container mt-5'>
+            <div className="accordion" id="accordionPanelsStayOpenExample">
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="panelsStayOpen-headingThree">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                            Formations
+                            FORMATIONS
                         </button>
                     </h2>
                     <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
@@ -93,9 +94,10 @@ export default function Formation() {
                     </div>
                 </div>
             </div>
+        </div>
 
-        )
-    }
+    )
+}
 
 
 
