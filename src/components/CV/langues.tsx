28a022072f28512b082ff3cd0c434a885cf72827
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-03-07 10:41:00 +0100

    fix function Langue sans 'return'

--- src/components/CV/langues.tsx
+++ src/components/CV/langues.tsx
@@ -1,57 +1,153 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-type TLangues = {
+// Typage de la table 'langues'
+type TLangue = {
     id: number;
     langue: string;
     niveau: string;
     user: {}
 }
 
-export default function CreateLangue() {
-    const [langues, setLangues] = useState <TLangues |undefined > ();
-    const [niveau, SetNiveau] = useState <TLangues |undefined >();
-    const [languesInput, setLanguesInput] = useState ("");
-    const [niveauInput, SetNiveauInput] = useState ("");
 
-    //useEffect (() => {
-    async function fetchData() {
-        const body = {
-            langue: languesInput,
-            niveau: niveauInput
-        }
+
+/** Foncton qui appelle:
+ * * **CreateLangue : fonction qui va utiliser le front pour faire un 'POST'.
+ * * **GetLangue    : fonction qui va utiloser le front pour faire un 'GET'.
+ * * **PatchLangue  : fonction qui va utiliser le front pour faire un 'PATCH'.
+ * * **DeleteLangue : fonction qui va utiliser le front pour faire un 'DELETE'.
+ */
+export default function Langue()
+{
+    const [ langue, setLangue ] = useState<TLangue | undefined>();
+    const [ niveau, SetNiveau ] = useState<TLangue | undefined>();
+    const [ langueInput, setLangueInput ] = useState("");
+    const [ niveauInput, SetNiveauInput ] = useState("");
+
+    const body = {
+        langue: langueInput,
+        niveau: niveauInput
+    }
+
+
+
+    // Création d'une langue : avec le 'create' du front.
+    async function CreateLangue()
+    {
         const requestOptions = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODExMzgwOSwiZXhwIjoxNjgzMTEzODA5fQ.SGeVdA_5QzUAfYmwy8dYn0MueTm6p6f7mVsbuGKReys'
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODExMzgwOSwiZXhwIjoxNjgzMTEzODA5fQ.SGeVdA_5QzUAfYmwy8dYn0MueTm6p6f7mVsbuGKReys'
             },
             body: JSON.stringify(body)
         };
-        const response = await fetch ('http://localhost:8000/langues',/*{ method: "POST" }*/ requestOptions);
+        const response = await fetch('http://localhost:8000/langues',/*{ method: "POST" }*/ requestOptions);
         const responseJson = await response.json();
 
-        if (!response.ok) {
+        if (!response.ok)
+        {
             // get error message from body or default to response status
             const error = (responseJson && responseJson.message) || response.status;
             return Promise.reject(error);
         }
         console.log("Success", responseJson);
 
-        setLangues(responseJson);
-        console.log(langues);
-        SetNiveau(responseJson);
-        console.log(niveau);        
+        setLangue(responseJson.langue);
+        SetNiveau(responseJson.niveau);
     }
-    //fetchData();
-//},[]);
-
-// Récupération de tous les langages:
-async function getLangue(){
-
-}
 
 
-///////////////////////return du 'GET'
+
+    // Récupération de tous les langages: avec le 'get' du front.
+    async function getLangue()
+    {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODExMzgwOSwiZXhwIjoxNjgzMTEzODA5fQ.SGeVdA_5QzUAfYmwy8dYn0MueTm6p6f7mVsbuGKReys'
+            },
+            body: JSON.stringify(body)
+        };
+        const response = await fetch('http://localhost:8000/langues',/*{ method: "GET" }*/ requestOptions);
+        const responseJson = await response.json();
+
+        if (!response.ok)
+        {
+            // get error message from body or default to response status
+            const error = (responseJson && responseJson.message) || response.status;
+            return Promise.reject(error);
+        }
+        console.log("Success", responseJson);
+
+        setLangue(responseJson.langue);
+        SetNiveau(responseJson.niveau);
+    }
+
+
+
+    // Modification d'une langue : avec le 'patch' du front.
+    async function PatchLangue()
+    {
+
+        const requestOptions = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODExMzgwOSwiZXhwIjoxNjgzMTEzODA5fQ.SGeVdA_5QzUAfYmwy8dYn0MueTm6p6f7mVsbuGKReys'
+            },
+            body: JSON.stringify(body)
+        };
+        const response = await fetch('http://localhost:8000/langues/',/*{ method: "PATCH" }*/ requestOptions)
+        const responseJson = await response.json();
+
+        if(!response.ok) {
+            // get error message from body or default to response status
+            const error = (responseJson && responseJson.message) || response
+            .status;
+            return Promise.reject (error);            
+        }
+        console.log ('success', responseJson);
+        setLangue(responseJson.langue);
+        SetNiveau(responseJson.niveau);
+    };
+
+
+
+    // Suppression d'une langue : avec le 'delete' du front.
+    async function DeleteLangue(){
+
+        const requestOptions = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Autorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODExMzgwOSwiZXhwIjoxNjgzMTEzODA5fQ.SGeVdA_5QzUAfYmwy8dYn0MueTm6p6f7mVsbuGKReys'
+            }
+        };
+
+        const response = await fetch ('http://localhost:8000/langues/',/*{ method: "DELETE" }*/ requestOptions);
+        const responseJson = await response.json();
+
+        if(!response.ok) {
+            // get error message from body or default to response status
+            const error = (responseJson && responseJson.message) || response
+            .status;
+            return Promise.reject (error);            
+        }
+        console.log ('success', responseJson);
+
+        setLangue(responseJson.langue);
+        SetNiveau(responseJson.niveau);
+    };
+
+
+    useEffect( () => {
+        getLangue();
+    }, []);
+
+
+
+    ///////////////////////return du 'GET'
 
     /* return (
         <div>
@@ -74,22 +170,22 @@ export { CreateLangue} */
 
     return (
         <div className='container mt-5'>
-        <div className="accordion-item">
-            <h2 className="accordion-header" id="panelsStayOpen-headingEight">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseEight" aria-expanded="false" aria-controls="panelsStayOpen-collapseEight">
-                    Langues
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseEight" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingEight">
-                <div className="btn-group mb-1 mt-2" role="group" aria-label="Third group">
-                    <button type="button" className="btn btn-info justify-content-end">+</button>
-                </div>
-                <div className="accordion-body">
-                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="panelsStayOpen-headingEight">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseEight" aria-expanded="false" aria-controls="panelsStayOpen-collapseEight">
+                        Langues
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseEight" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingEight">
+                    <div className="btn-group mb-1 mt-2" role="group" aria-label="Third group">
+                        <button type="button" className="btn btn-info justify-content-end">+</button>
+                    </div>
+                    <div className="accordion-body">
+                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
     );
 }
-    export { CreateLangue}
+export { Langue }
