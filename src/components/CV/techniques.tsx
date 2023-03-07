924e1db9a78c546ef868bed45e1f562717b9ecbf
WebToLaure <lmarmus@hotmail.fr>
2023-03-07 13:08:24 +0100

    feat fonctionnalités

--- src/components/CV/techniques.tsx
+++ src/components/CV/techniques.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 
 
 type TTech = {
+    map(arg0: (item: any) => JSX.Element): import('react').ReactNode;
     id: number;
     libelle: string;
     user: {}
@@ -23,7 +24,7 @@ export default function Technique() {
 
         const requestOptions = {
             method: 'POST',
-             headers: {
+            headers: {
                 'Content-Type': 'application/json',
                 Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvcGhpZUBnbWFpbC5mciIsInN1YiI6MSwiaWF0IjoxNjc3MjQ3NDg0LCJleHAiOjE2ODIyNDc0ODR9.XUDUNkBZiqT3fUmdn9IDW5K2kb2BegVxDZpMMNUQ_U4'
             },
@@ -125,29 +126,58 @@ export default function Technique() {
                             COMPETENCES TECHNIQUES
                         </button>
                     </h2>
+                    {/* <!-- Get All Compétences Techniques --> */}
                     <div id="panelsStayOpen-collapseFive" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFive">
                         <div id="collapseFive" className="accordion-collapse collapse show" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                             <div className="accordion-body">
-                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <div className="p-2">
+                                    <table className="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Libellé</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {technique?.map((item) => (
+                                                <tr>
+                                                    <th scope="row">{item.id}</th>
+                                                    <td>{item?.libelle}</td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+
                             </div>
-                            <div className="btn-group mb-1 mt-2 ms-2" role="group" aria-label="Third group">
+
+                            <div className="btn-group mb-2 mt-2 ms-2" role="group" aria-label="Third group">
+                                {/* <!-- Add button --> */}
                                 <button type="button" className="btn btn-outline-info btn-rounded-floating" data-mdb-ripple-color="dark">
                                     <i className="bi bi-plus"></i>
                                 </button>
+
+                                <h5 className="Ajouter une compétence">Ajout</h5>
+                                <input type='text' onChange={(event) => setTechInput(event.target.value)}></input>
+                                <button onClick={() => createTechnique()}>Valider</button>
+
+
+                                {/* <!-- Update button --> */}
                                 <button type="button" className="btn btn-outline-warning btn-rounded-floating" data-mdb-ripple-color="dark" >
                                     <i className="bi bi-pencil"></i>
                                 </button>
+                                {/* <!-- Delete button --> */}
                                 <button type="button" className="btn btn-outline-danger btn-rounded-floating" data-mdb-ripple-color="dark" >
                                     <i className="bi bi-trash3"></i>
                                 </button>
                             </div>
 
+
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
+        </div>
 
 
     );
