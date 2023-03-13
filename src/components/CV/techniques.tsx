bcef067739122fc68950579ecc8cafe403dcf4d8
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-03-13 16:47:01 +0100

    récupération du bon fichier techniques

--- src/components/CV/techniques.tsx
+++ src/components/CV/techniques.tsx
@@ -2,22 +2,22 @@ import { useState, useEffect } from 'react';
 
 
 
+
+
 type TTech = {
-    map(arg0: (item: any) => JSX.Element): import('react').ReactNode;
     id: number;
     libelle: string;
-    user: {}
 }
 
 export default function Technique() {
 
-    const [technique, setTechnique] = useState<TTech | undefined>();
-    const [techInput, setTechInput] = useState("")
+    const [technique, setTechnique] = useState<TTech[]>([]);//array qui contient des techniques
+    const [techInput, setTechInput] = useState<string>("");
 
-    const body = {
-
-        libelle: techInput
-    }
+    /*  const body = {
+ 
+         libelle: techInput
+     } */
 
     //useEffect(async() => {
     async function createTechnique() {
@@ -26,104 +26,88 @@ export default function Technique() {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvcGhpZUBnbWFpbC5mciIsInN1YiI6MSwiaWF0IjoxNjc3MjQ3NDg0LCJleHAiOjE2ODIyNDc0ODR9.XUDUNkBZiqT3fUmdn9IDW5K2kb2BegVxDZpMMNUQ_U4'
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODY5NzA5NiwiZXhwIjoxNjgzNjk3MDk2fQ.KuPHnEt0EiYAA9jEVxfR2Vvj95oWOYEvuuEqlRSKtGw'
             },
-            body: JSON.stringify(body)
+            body: JSON.stringify({
+
+                libelle: techInput
+            })
         };
 
         const response = await fetch('http://localhost:8000/techniques', /*{ method: "POST" }*/requestOptions);
 
         const responseJson = await response.json();
 
-        if (!response.ok) {
-            // get error message from body or default to response status
-            const error = (responseJson && responseJson.message) || response.status;
-            return Promise.reject(error);
-        }
         console.log("Success!", responseJson);
 
-        setTechnique(responseJson);
+        setTechnique([...technique, responseJson]);
+        setTechInput("");
     };
     async function getTechnique() {
-
         const requestOptions = {
             method: 'GET',
             headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvcGhpZUBnbWFpbC5mciIsInN1YiI6MSwiaWF0IjoxNjc3MDA1MDc3LCJleHAiOjE2ODIwMDUwNzd9.zkOyUiBggcgOtdOdNzwI4orxX-gV5fMmEacWqZn4Zk4'
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODY5NzA5NiwiZXhwIjoxNjgzNjk3MDk2fQ.KuPHnEt0EiYAA9jEVxfR2Vvj95oWOYEvuuEqlRSKtGw'
             },
-
-            body: JSON.stringify(body)
         };
-
-
         const response = await fetch('http://localhost:8000/techniques', requestOptions)
         const responseJson = await response.json();
-
-        if (!response.ok) {
-            // get error message from body or default to response status
-            const error = (responseJson && responseJson.message) || response.status;
-            return Promise.reject(error);
-        }
+        console.log(responseJson);
         console.log("Success!", responseJson);
-
-        setTechnique(responseJson.technique);
+        setTechnique(responseJson);
 
     };
 
     async function patchTechnique() {
-
         const requestOptions = {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvcGhpZUBnbWFpbC5mciIsInN1YiI6MSwiaWF0IjoxNjc3MDA1MDc3LCJleHAiOjE2ODIwMDUwNzd9.zkOyUiBggcgOtdOdNzwI4orxX-gV5fMmEacWqZn4Zk4'
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODY5NzA5NiwiZXhwIjoxNjgzNjk3MDk2fQ.KuPHnEt0EiYAA9jEVxfR2Vvj95oWOYEvuuEqlRSKtGw'
             },
-            body: JSON.stringify(body)
-        };
+            body: JSON.stringify({
 
+                libelle: techInput
+            })
+        };
         const response = await fetch('http://localhost:8000/techniques/', requestOptions)
         const responseJson = await response.json();
-        if (!response.ok) {
-            // get error message from body or default to response status
-            const error = (responseJson && responseJson.message) || response.status;
-            return Promise.reject(error);
-        }
-        console.log("Success!", responseJson);
 
-        setTechnique(responseJson.technique);
+        console.log("Success!", responseJson);
+        setTechnique(responseJson);
     };
+
     async function deleteTechnique() {
         const requestOptions = {
             method: 'DELETE',
             headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvcGhpZUBnbWFpbC5mciIsInN1YiI6MSwiaWF0IjoxNjc3MDA1MDc3LCJleHAiOjE2ODIwMDUwNzd9.zkOyUiBggcgOtdOdNzwI4orxX-gV5fMmEacWqZn4Zk4'
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlNhbmR5QkBnbWFpbC5mciIsInN1YiI6MTgsImlhdCI6MTY3ODY5NzA5NiwiZXhwIjoxNjgzNjk3MDk2fQ.KuPHnEt0EiYAA9jEVxfR2Vvj95oWOYEvuuEqlRSKtGw'
             }
         };
 
         const response = await fetch('http://localhost:8000/techniques/', requestOptions)
         const responseJson = await response.json()
-        if (!response.ok) {
-            // get error message from body or default to response status
-            const error = (responseJson && responseJson.message) || response.status;
-            return Promise.reject(error);
-        }
         console.log("Success!", responseJson);
-
-        setTechnique(responseJson.technique);
+        setTechnique(responseJson);
     }
 
     useEffect(() => {
-
         getTechnique();
-
     }, []);
+
+    const listTechnique = technique?.map((item) => (
+        <li>
+            <p>{item?.libelle}</p>
+        </li>
+    ))
+
     return (
         <div className='container mt-5'>
             <div className="accordion" id="accordionPanelsStayOpenExample">
                 <div className="accordion-item ms-4 me-4">
                     <h2 className="accordion-header" id="panelsStayOpen-headingFive">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFive" aria-expanded="false" aria-controls="panelsStayOpen-collapseFive">
-                        <div className= "position-absolute top-50 start-50 translate-middle text-center">  COMPÉTENCES TECHNIQUES</div>
+                            <div className="position-absolute top-50 start-50 translate-middle text-center">  COMPÉTENCES TECHNIQUES</div>
                         </button>
                     </h2>
                     {/* <!-- Get All Compétences Techniques --> */}
@@ -131,23 +115,12 @@ export default function Technique() {
                         <div id="collapseFive" className="accordion-collapse collapse show" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                             <div className="accordion-body">
                                 <div className="p-2">
-                                    <table className="table table-bordered">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">Libellé</th>
-                                            </tr>
-                                        </thead>
+                                    <ul>
                                         <tbody>
-                                            {technique?.map((item) => (
-                                                <tr>
-                                                    <th scope="row">{item.id}</th>
-                                                    <td>{item?.libelle}</td>
-                                                </tr>
-                                            ))}
+                                            {listTechnique}
                                         </tbody>
-                                    </table>
+                                    </ul>
                                 </div>
-
                             </div>
 
                             <div className="btn-group mb-2 mt-2 ms-2" role="group" aria-label="Third group">
@@ -155,7 +128,7 @@ export default function Technique() {
                                 <button type="button" className="btn btn-outline-info btn-rounded-floating" data-mdb-ripple-color="dark">
                                     <i className="bi bi-plus"></i>
                                 </button>
-                                <input type='text' placeholder ="Votre compétence ici" onChange={(event) => setTechInput(event.target.value)}></input>
+                                <input type='text' value={techInput} placeholder="Votre compétence ici" onChange={(event) => setTechInput(event.target.value)}></input>
                                 <button onClick={() => createTechnique()}>Valider</button>
 
 
