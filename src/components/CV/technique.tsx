fa73ff9ef7bb9e7fce4510a70bbc8c3384c8dfe7
WebToLaure <lmarmus@hotmail.fr>
2023-03-18 19:37:23 +0100

    feat: update /delete fonctionnels sur component techniques

--- src/components/CV/technique.tsx
+++ src/components/CV/technique.tsx
@@ -10,7 +10,7 @@ export default function Technique(props: any) {
     /*   console.log(props); */
 
     const [techInput, setTechInput] = useState<string>("");
-
+    const [showInput, setShowInput] = useState(false);
 
     const token = useContext(AuthContext).user?.access_token;
     /*  console.log("test ", token); */
@@ -30,8 +30,14 @@ export default function Technique(props: any) {
         };
         const response = await fetch(`http://localhost:8000/techniques/${props.item.id}`, requestOptions)
         const responseJson = await response.json();
-        /* console.log("Success!", responseJson); */
-        setTechInput(responseJson);
+        console.log("Success!", responseJson);
+
+        if (responseJson.statusCode === 200) {
+            props.patch(responseJson.data)
+            setShowInput(false);
+            console.log(props.patch);
+            
+        }
     };
 
 
@@ -47,8 +53,14 @@ export default function Technique(props: any) {
         /* console.log("Success!", responseJson); */
         if (responseJson.statusCode === 200) {
             props.del(props.item.id)
+          
         }
-    };
+    }
+
+
+    function update() {
+        setShowInput(true)
+    }
 
 
     return (
@@ -58,33 +70,42 @@ export default function Technique(props: any) {
                     <li className="hardSkills">
                         {props.item?.libelle}
                     </li >
+                    {showInput && <div>
+                        <hr />
+                        <input type='text' className="form-control" value={techInput} placeholder="Modifiez votre compétence ici" onChange={(event) => setTechInput(event.target.value)} aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+                        <button onClick={patchTechnique} type="button" className="btn btn-outline-success btn-sm" data-mdb-ripple-color="dark" >
+                            <i className="bi bi-check-circle-fill"></i>
+                        </button>
+                    </div>}
                 </div>
+
                 <div className="col">
                     <div className="btn-group mb-2 mt-2 ms-5" role="group" aria-label="Third group">
                         {/* <!-- Update button --> */}
-                         {/* <button onClick={() => patchTechnique()} type="button" className="btn" data-mdb-ripple-color="dark" >
+                        {/* <button onClick={() => patchTechnique()} type="button" className="btn" data-mdb-ripple-color="dark" >
                         <PencilIcon size ={20}
                             onMouseEnter = {()=>setIsTrashHovered(true)}
                             onMouseLeave = {()=> setIsTrashHovered(false)}
                             style = {{color:isTrashHovered? "#e9c131": "#b8b8b8"}}/>
                         </button> */}
-                         {/* <!-- Delete button -->  */}
-                         {/*  <button onClick={deleteTechnique} type="button" className="btn" data-mdb-ripple-color="dark" >
+                        {/* <!-- Delete button -->  */}
+                        {/*  <button onClick={deleteTechnique} type="button" className="btn" data-mdb-ripple-color="dark" >
                             <TrashIcon size ={20}
                             onMouseEnter = {()=>setIsTrashHovered(true)}
                             onMouseLeave = {()=> setIsTrashHovered(false)}
                             style = {{color:isTrashHovered? "#be0808": "#b8b8b8"}}/>
                         </button>   */}
-                         <button onClick={patchTechnique} type="button" className="btn btn-outline-warning btn-rounded-floating ms-1" data-mdb-ripple-color="dark" >
+
+                        <button onClick={update} type="button" className="btn btn-outline-warning btn-rounded-floating ms-1" data-mdb-ripple-color="dark" >
                             <i className="bi bi-pencil"></i>
                         </button>
 
                         {/* bouton supprimer */}
                         <button onClick={deleteTechnique} type="button" className="btn btn-outline-danger btn-rounded-floating ms-1" data-mdb-ripple-color="dark" >
                             <i className="bi bi-trash3"></i>
-                        </button> 
+                        </button>
                     </div>
-                </div> {/* btn-outline-danger btn-rounded-floating  */}
+                </div>
             </div>
         </div>
     )
