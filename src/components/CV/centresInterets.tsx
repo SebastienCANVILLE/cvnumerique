25e0374b4cd470fa98d6c2c7a4bb01fcbd07ad77
WebToLaure <lmarmus@hotmail.fr>
2023-03-18 19:33:41 +0100

    feat : C.I.delete fonctionnel

--- src/components/CV/centresInterets.tsx
+++ src/components/CV/centresInterets.tsx
@@ -1,19 +1,25 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { AuthContext } from '../../context/authContext';
-import Interet from './centreInteret';
+import CentreInteret from "./centreInteret";
 
 
 type TCInt = {
     id: number,
     intitule: string
-
 }
+
 export default function CentreInterets() {
 
-    const [interet, setInteret] = useState<TCInt[]>([]);
+    /* const [interet, setInteret] = useState<TCInt[]>([]); */
     const [intInput, setIntInput] = useState<string>("");
+
     //POST request fetch inside useEffect React hooks
+
     const token = useContext(AuthContext).user?.access_token;
+    const user = useContext(AuthContext).user?.user;
+
+    const test = useContext(AuthContext).user;
+    const setUser = useContext(AuthContext).setUser;
     async function createInteret() {
 
         const requestOptions = {
@@ -26,17 +32,14 @@ export default function CentreInterets() {
                 intitule: intInput
             })
         };
-
         const response = await fetch('http://localhost:8000/interets', /*{ method: "POST" }*/requestOptions);
-
         const responseJson = await response.json();
-
         console.log("Success!", responseJson);
-
-        setInteret([...interet, responseJson]);
+        test!.user.centres_interets = [...test!.user.centres_interets, responseJson.data]
+        setUser({ ...test! });
         setIntInput("");
     };
-    async function getInteret() {
+    /* async function getInteret() {
         const requestOptions = {
             method: 'GET',
             headers: {
@@ -53,12 +56,24 @@ export default function CentreInterets() {
     useEffect(() => {
         getInteret();
     }, []);
-
-    function deleteInteret(id: number) {
-        const newInteret = interet.filter(item => item.id !== id)
-        setInteret(newInteret);
+ */
+    function patchInteret(item: TCInt) {
+        /* const interet = test!.user.centres_interets.filter((elm) => elm.id !== item.id);
+        const newInteret= [...interet, item];
+        test!.user.centres_interets = newInteret;
+        setUser({...test!}); */
+        const index = test!.user.centres_interets.findIndex(elm => elm.id === item.id);
+        test!.user.centres_interets[index] = item;
+        setUser({ ...test! });
     }
-    const listInteret = interet?.map(item => <Interet del={deleteInteret} item={item} key={item.id} />)
+    function deleteInteret(id: number) {
+        const interet = test!.user.centres_interets.filter(item => item.id !== id);
+        test!.user.centres_interets = interet;
+        console.log(test);
+
+        setUser({ ...test! });
+    }
+    const listInteret = user?.centres_interets?.map(item => <CentreInteret del={deleteInteret} patch={patchInteret} item={item} key={item.id} />)
 
     return (
 
@@ -66,7 +81,7 @@ export default function CentreInterets() {
             <div className="accordion" id="accordionPanelsStayOpenExample">
                 <div className="accordion-item ms-4 me-4">
                     <h2 className="accordion-header" id="panelsStayOpen-headingNine">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseNine" aria-expanded="false" aria-controls="panelsStayOpen-collapseNine">
+                        <button className="accordion-button collapsed shadow p-3  bg-body-tertiary rounded" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseNine" aria-expanded="false" aria-controls="panelsStayOpen-collapseNine">
                             <div className="position-absolute top-50 start-50 translate-middle text-center"> CENTRES D'INTÉRÊTS</div>
                         </button>
                     </h2>
@@ -80,12 +95,12 @@ export default function CentreInterets() {
                                         {listInteret}
                                     </div>
                                 </div>
-                            <div className="input-group mb-3">
-                                <input type='text' className="form-control" value={intInput} placeholder="Votre Centre d'intérêt" onChange={(event) => setIntInput(event.target.value)} aria-label="Recipient's username" aria-describedby="button-addon2"></input>
-                                <button onClick={() => createInteret()} type="button" className="btn btn-outline-info" data-mdb-ripple-color="dark">
-                                    <i className="bi bi-plus"></i>
-                                </button>
-                            </div>
+                                <div className="input-group mb-3">
+                                    <input type='text' className="form-control" value={intInput} placeholder="Votre Centre d'intérêt" onChange={(event) => setIntInput(event.target.value)} aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+                                    <button onClick={() => createInteret()} type="button" className="btn btn-outline-info" data-mdb-ripple-color="dark">
+                                        <i className="bi bi-plus"></i>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
