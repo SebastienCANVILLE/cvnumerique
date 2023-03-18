db0da0ff7cc8ecc636d49795a5f2cbc09ab2f09b
WebToLaure <lmarmus@hotmail.fr>
2023-03-18 19:35:06 +0100

    feat : update fonctionnel  sur component formations

--- src/components/CV/formations.tsx
+++ src/components/CV/formations.tsx
@@ -4,7 +4,7 @@ import Formation from './formation';
 
 type TForm = {
     id: number,
-    specialite: string,
+    lieu_formation: string,
     diplôme: string,
     date_obtention: Date,
 }
@@ -35,7 +35,7 @@ export default function Formations() {
                 Authorization: `Bearer ${token}`
             },
             body: JSON.stringify({
-                specialite: formInput,
+                lieu_formation: formInput,
                 diplôme: diplomeInput,
                 date_obtention: dateInput
             })
@@ -84,6 +84,13 @@ export default function Formations() {
    useEffect(() => {
        getFormations();
    }, []);  */
+   function patchFormation(item: TForm) {
+    const index = test!.user.formations.findIndex(elm => elm.id === item.id);
+    test!.user.formations[index] = item;
+    setUser({ ...test! });
+
+
+   }
     function deleteFormation(id: number) {
         /* const newTechnique = technique.filter(item => item.id !== id)
         setTechnique(newTechnique); */
@@ -94,7 +101,7 @@ export default function Formations() {
         setUser({ ...test! });
 
     }
-    const listFormation = user?.formations?.map(item => <Formation del={deleteFormation} item={item} key={item.id} />)
+    const listFormation = user?.formations?.map(item => <Formation del={deleteFormation} patch={patchFormation} item={item} key={item.id} />)
     console.log(listFormation);
     console.log(user);
 
@@ -106,33 +113,39 @@ export default function Formations() {
         <div className='container mt-5'>
             <div className="accordion" id="accordionPanelsStayOpenExample">
                 <div className="accordion-item ms-4 me-4">
-                    <h2 className="accordion-header" id="panelsStayOpen-headingThree">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                            <div className="position-absolute top-50 start-50 translate-middle text-center"> FORMATIONS</div>
+                    <h2 className="accordion-header " id="panelsStayOpen-headingThree">
+                        <button className="accordion-button collapsed shadow p-3  bg-body-tertiary rounded" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                            <div className="position-absolute top-50 start-50 translate-middle text-center">  FORMATIONS</div>
                         </button>
                     </h2>
 
                     {/* <!-- Get All Formations --> */}
                     <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
                         <div id="collapseThree" className="accordion-collapse collapse show" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                        </div>
-                        <ul>
-                            {listFormation}
-                        </ul>
+                            <div className="accordion-body">
+                                <div className="p-2">
+                                    <div className="col">
+                                        {listFormation}
+                                    </div>
+                                </div>
 
-                        {/* <!-- Add Formations button --> */}
-                        <div className="btn-group mb-2 mt-2 ms-2 me -2" role="group" aria-label="Third group">
-                            <input type='text' value={formInput} placeholder="Saisir votre formation" onChange={(event) => setFormInput(event.target.value)}></input>
-                            <input type='text' value={diplomeInput} placeholder="Saisir votre diplôme" onChange={(event) => setDiplomeInput(event.target.value)}></input>
-                            <input type='text' value={dateInput} placeholder="Date d'obtention" onChange={(event) => setDateInput(event.target.value)}></input>
-                            <button onClick={() => createFormation()} type="button" className="btn btn-outline-info btn-rounded-floating" data-mdb-ripple-color="dark">
-                                <i className="bi bi-plus"></i>
-                            </button>
+
+                                {/* <!-- Add Formations button --> */}
+                                <div className="input-group mb-3">
+                                    <input type='text' className="form-control" value={formInput} placeholder="Lieu de formation" onChange={(event) => setFormInput(event.target.value)}aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+                                    <input type='text' className="form-control" value={diplomeInput} placeholder="Diplôme obtenu" onChange={(event) => setDiplomeInput(event.target.value)}aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+                                    <input type='text' className="form-control" value={dateInput} placeholder="Date d'obtention" onChange={(event) => setDateInput(event.target.value)}aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+                                    <button onClick={() => createFormation()} type="button" className="btn btn-outline-info btn-rounded-floating" data-mdb-ripple-color="dark">
+                                        <i className="bi bi-plus"></i>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
